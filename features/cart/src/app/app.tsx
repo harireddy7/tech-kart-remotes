944a380e2c71b806505c25ac1cab943a5708af08
harireddy7 <harimsd8@gmail.com>
2023-12-17 12:49:15 +0530

    fix: add hotfix to cart

--- features/cart/src/app/app.tsx
+++ features/cart/src/app/app.tsx
@@ -2,8 +2,11 @@ import { Box, Typography } from '@mui/material';
 
 export function App() {
   return (
-    <Box p={2} sx={{ background: '#cceeff', textAlign: 'center' }}>
-      <Typography variant='h4'>Cart</Typography>
+    <Box p={2} sx={{ textAlign: 'center' }}>
+      <Typography variant='h4' p={2} sx={{ background: '#d7ec9c', textAlign: 'center' }}>Cart</Typography>
+      <Typography p={2}>
+        Adding hotfix to cart-v1.0.0
+      </Typography>
     </Box>
   );
 }
