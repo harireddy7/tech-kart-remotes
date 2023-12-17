b085f7b7028b53ed87351571a8d9c00c21d58bbd
harireddy7 <harimsd8@gmail.com>
2023-12-17 12:04:46 +0530

    feat: add cart feature ui

---
+++ features/cart/src/app/app.tsx
@@ -0,0 +1,11 @@
+import { Box, Typography } from '@mui/material';
+
+export function App() {
+  return (
+    <Box p={2} sx={{ background: '#cceeff', textAlign: 'center' }}>
+      <Typography variant='h4'>Cart</Typography>
+    </Box>
+  );
+}
+
+export default App;
