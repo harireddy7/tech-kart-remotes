e18ed90825d336fcc9b8bcada486a1a5f7736720
harireddy7 <harimsd8@gmail.com>
2024-01-06 19:58:31 +0530

    feat: add new tag ui lib

---
+++ libs/ui/tag/src/lib/tag.tsx
@@ -0,0 +1,16 @@
+import { Typography } from '@mui/material';
+import './tag.css';
+
+export interface TagProps {
+  title: string;
+}
+
+export function Tag({ title }: TagProps) {
+  return (
+    <Typography variant='h5' color='red' pt={2}>
+      👉 Tag.tsx from {title}
+    </Typography>
+  );
+}
+
+export default Tag;
