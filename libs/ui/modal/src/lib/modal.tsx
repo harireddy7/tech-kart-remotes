a550cda96704462548b78ecf6e8e4a172e52ef37
harireddy7 <harimsd8@gmail.com>
2024-01-08 18:28:28 +0530

    feat: add modal lib

---
+++ libs/ui/modal/src/lib/modal.tsx
@@ -0,0 +1,14 @@
+import './modal.css';
+
+/* eslint-disable-next-line */
+export interface ModalProps {}
+
+export function Modal(props: ModalProps) {
+  return (
+    <div>
+      <h1>Welcome to Modal!</h1>
+    </div>
+  );
+}
+
+export default Modal;
