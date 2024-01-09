bc8a0c6bac7b0d6573c2f6221cc0f9738f890b99
harireddy7 <harimsd8@gmail.com>
2024-01-09 13:01:17 +0530

    feat: add libraries under libs/ui/*

---
+++ libs/ui/tag/src/lib/tag.tsx
@@ -0,0 +1,14 @@
+import './tag.css';
+
+/* eslint-disable-next-line */
+export interface TagProps {}
+
+export function Tag(props: TagProps) {
+  return (
+    <div>
+      <h1>Welcome to Tag!</h1>
+    </div>
+  );
+}
+
+export default Tag;
