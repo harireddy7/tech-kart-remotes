19493e0c1efb5c305b318717c7e64e61833e2e2e
harireddy7 <harimsd8@gmail.com>
2024-01-14 22:23:56 +0530

    test: add msw and json-server

---
+++ features/products/src/mocks/handlers.ts
@@ -0,0 +1,16 @@
+import { rest } from 'msw';
+
+const host = 'http://localhost:3000';
+
+export const handlers = [
+  rest.get(`${host}/products`, (_, res, ctx) => {
+    console.log('in msw handler');
+    return res(
+      ctx.status(200),
+      ctx.json([{ name: 'barry' }, { name: 'oliver' }]),
+      ctx.set({
+        'Access-Control-Allow-Origin': 'localhost'
+      })
+    );
+  }),
+];
