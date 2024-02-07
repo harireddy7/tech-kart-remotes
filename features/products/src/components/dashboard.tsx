6fc2a420dfcdc485b5185bc174cbb429f324a7a4
harireddy7 <harimsd8@gmail.com>
2024-02-07 10:14:29 +0530

    fix: remove unused variables

--- features/products/src/components/dashboard.tsx
+++ features/products/src/components/dashboard.tsx
@@ -1,17 +1,12 @@
 import { Box, Typography } from '@mui/material';
 import styled from 'styled-components';
 import { Card } from '@ui/card';
-// import { getUniqueId } from '@ext-tech-kart/utils';
-import { GlobalType } from '@utils/types/global';
 
 const Wrapper = styled(Box)`
   border: 1px solid #ccc;
 `;
 
 const Dashboard = () => {
-  const feature: GlobalType = {
-    feature: 'products',
-  };
   return (
     <Box p={2}>
       <Box p={2} mx='auto'>
